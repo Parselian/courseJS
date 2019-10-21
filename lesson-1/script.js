d58cea7cb1d5d031e9ed2ef518f2d8324692b845
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-10-21 17:57:01 +0300

    сделал основные таски

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,31 +1,46 @@
 /* УРОК №1 */
-let money = 15152,
+let money = +prompt('Введите ваш месячный доход:', ''),
     income = 'фриланс',
-    addExpenses = 'курсы, МузЫка, игры',
-    deposit = true,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую: ', ''),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 300000,
     period = 10;
 
-/*
-alert('Привет, бренный мир :с');
-console.log('мама, я прогроммист!'); 
-*/
+    
+    
+/* УРОК №3 */
+let arrExpenses = addExpenses.split(', ');
 
-
-
-/* УРОК №2 */
+console.log(arrExpenses);
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-console.log(income.length);
-console.log('Период ' + period + ' месяцев');
-console.log('цель заработать ' + mission + ' шекелей');
 
-let arrExpenses = addExpenses.toLowerCase().split(', ');
-console.log(arrExpenses);
+let question1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    question2 = +prompt('Во сколько это обойдется?', ''),
+    question3 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    question4 = +prompt('Во сколько это обойдется?', ''),
+    budgetMonth,
+    roadToMission;
 
-let budgetDay = money / 30;
+budgetMonth = money - (question2 + question4);
+console.log(budgetMonth);
 
-console.log('Ваш бюджет на день: ' + Math.trunc(budgetDay) + ' шекелей, остаток: ' + (money % 30)); 
+roadToMission = Math.ceil(mission / budgetMonth);
+console.log(roadToMission);
+
+let budgetDay = budgetMonth / 30;
+console.log('Ваш бюджет на день: ' + Math.floor(budgetDay)); 
+
+if( budgetDay > 800 ) {
+  alert('Высокий уровень дохода');
+} else if(300 < budgetDay < 800 || budgetDay === 300 || budgetDay === 800 ) {
+  alert('Средний уровень дохода');
+} else if( 0 < budgetDay < 300 || budgetDay === 0 || budgetDay === 300 ) {
+  alert('Низкий уровень дохода');
+} else {
+  alert('Что-то пошло не так');
+}
+
 
 
