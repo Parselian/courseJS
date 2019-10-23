ce7ba0be2da6a1416168f49f3c714b12ca0aaf25
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-10-23 22:32:20 +0300

    удалил лишние логи

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -94,19 +94,17 @@ console.log(getTargetMonth());
 let budgetDay = 0;
 
 function getStatusIncome() {
-  console.log('accumulatedMonth: ', accumulatedMonth);
-  console.log('budgetDay: ', budgetDay);
   budgetDay = Math.floor(accumulatedMonth / 30);
 
   
 
   if( budgetDay > 800 ) {
     return 'Высокий уровень дохода';
-  } else if( budgetDay >= 300 && budgetDay <= 800 ) {
+  } else if( 300 <= budgetDay <= 800 ) {
     return 'Средний уровень дохода';
-  } else if( budgetDay > 0 && budgetDay <= 300 ) {
+  } else if( 0 <= budgetDay <= 300 ) {
     return 'Низкий уровень дохода';
-  } else if( budgetDay <= 0 ) {
+  } else if( budgetDay < 0 ) {
     return 'Что-то пошло не так';
   }
 }
