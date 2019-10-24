614328ef9eb5d1d696df9c15d46086f94e87fb51
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-10-24 12:59:34 +0300

    нормально написал вторую усложнёнку

--- lesson-5_hard/script.js
+++ lesson-5_hard/script.js
@@ -25,62 +25,84 @@ filterNumbers();
 
 
 /*  ЗАДАНИЕ 2  */
+function findPrimeNums(num) {
 
-function showSimpleNums() {
-  let i,
-      arr = [],
-      x = 0;
-
-  /* ИЗБАВЛЯЕМСЯ ОТ ЧЕТНЫХ */
-  for( i = 1; i <= 100; i++ ) {
-    x++;
-    if(i !== 2 && i % 2 === 0) {
-      continue;           
-    } else {
-      // console.log(i);
-      arr[x] = i;
+  if(num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if( num % i == 0 ) {
+      return false;
     }
   }
-
-  x = 0;
-
-  /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 3 */
-  for (i = 1; i <= 100; i++ ) {
-    x++;
-    if( arr[i] !== 3 && arr[i] % 3 === 0 ) {
-      delete arr[i];
-    } else {
-      // console.log(arr[i]);
-    }
-  }
-
-  x = 0;
-
-  /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 5 */
-  for (i = 1; i <= 100; i++ ) {
-    x++;
-    if(arr[i] !== 5 && arr[i] % 5 === 0 ) {
-      delete arr[i];
-    } else {
-      // console.log(arr[i]);
-    }
-  }
-
-  x = 0;
-
-  /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 7 */
-  for (i = 1; i <= 100; i++ ) {
-    x++;
-    if( arr[i] !== 7 && arr[i] % 7 === 0 ) {
-      delete arr[i];
-    } else {
-      console.log(arr[i]);
-    }
-  }
-
-  // console.log(arr.length);
-  console.log(arr);
+  return true;
 }
 
+for(let i = 1; i < 100; i++) {
+  if(findPrimeNums(i)) {
+    console.log(i);
+  }
+}
 
-showSimpleNums();
+findPrimeNums();
+
+
+
+// function showSimpleNums() {
+//   let i,
+//       arr = [],
+//       x = 0;
+
+//   /* ИЗБАВЛЯЕМСЯ ОТ ЧЕТНЫХ */
+//   for( i = 1; i <= 100; i++ ) {
+//     x++;
+//     if(i !== 2 && i % 2 === 0) {
+//       continue;           
+//     } else {
+//       // console.log(i);
+//       arr[x] = i;
+//     }
+//   }
+
+//   x = 0;
+
+//   /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 3 */
+//   for (i = 1; i <= 100; i++ ) {
+//     x++;
+//     if( arr[i] !== 3 && arr[i] % 3 === 0 ) {
+//       delete arr[i];
+//     } else {
+//       // console.log(arr[i]);
+//     }
+//   }
+
+//   x = 0;
+
+//   /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 5 */
+//   for (i = 1; i <= 100; i++ ) {
+//     x++;
+//     if(arr[i] !== 5 && arr[i] % 5 === 0 ) {
+//       delete arr[i];
+//     } else {
+//       // console.log(arr[i]);
+//     }
+//   }
+
+//   x = 0;
+
+//   /* ИЗБАВЛЯЕМСЯ ОТ КРАТНЫХ 7 */
+//   for (i = 1; i <= 100; i++ ) {
+//     x++;
+//     if( arr[i] !== 7 && arr[i] % 7 === 0 ) {
+//       delete arr[i];
+//     } else {
+//       console.log(arr[i]);
+//     }
+//   }
+
+//   // console.log(arr.length);
+//   console.log(arr);
+// }
+
+
+// showSimpleNums();
