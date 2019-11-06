508e8216dcc6ad5c1a5ec4f9df3c42a1eb5c29b3
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-06 09:41:28 +0300

    сделал первое дополнительное 10-го урока

--- first-task/script.js
+++ first-task/script.js
@@ -23,21 +23,54 @@ DomElement.prototype.createElement = function() {
   }
 
   elem.style.cssText = `
+    position: absolute;
     width: ` + this.width + `;
     height: ` + this.height + `;
     background: ` + this.bg + `;
     font-size: ` + this.fontSize + `;
+    cursor: pointer;
   `;
 
-  elem.textContent = 'Любой текст';
-  
+  // elem.textContent = 'Любой текст';
   body.appendChild(elem);
+
+  document.addEventListener('keydown', function() {
+    let keycode;
+    if(event.which) {
+      keycode = event.which;
+    }
+    let computedStyle = getComputedStyle(elem),
+        posLeft = parseFloat(computedStyle.left),
+        posRight = parseFloat(computedStyle.right),
+        posTop = parseFloat(computedStyle.top),
+        posBottom = parseFloat(computedStyle.bottom);
+    
+    switch(keycode) {
+      case 37:
+        elem.style.right = posRight + 10 + 'px';
+        elem.style.left = posLeft - 10 + 'px';
+        console.log(posLeft);
+        break;
+      case 38:
+        elem.style.top = posTop - 10 + 'px';
+        elem.style.bottom = posBottom + 10 + 'px';
+        break;
+      case 39:
+        elem.style.left = posLeft + 10 + 'px';
+        elem.style.right = posRight - 10 + 'px';
+        break;
+      case 40:
+        elem.style.top = posTop + 10 + 'px';
+        elem.style.bottom = posBottom - 10 + 'px';
+        break;
+    }
+  });
 };
 
-let newElem = new DomElement('#d', '100px', '200px', 'green', '14px');
+let newElem = new DomElement('#d', '100px', '100px', 'green', '14px');
 
-newElem.createElement();
+document.addEventListener('DOMContentLoaded', function() {
+  newElem.createElement();
+});
 
-// console.dir(DomElement);
-// DomElement.prototype.createElement();
-console.log(newElem);
+
