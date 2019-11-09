9fc5aaa727efd8a7d3bb7dd35fdf06aa3e3be130
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-10 00:16:05 +0300

    тест

--- 3DGlo/script/script.js
+++ 3DGlo/script/script.js
@@ -4,14 +4,19 @@ window.addEventListener('DOMContentLoaded', () => {
     const timerHours = document.querySelector(hour),
           timerMinutes = document.querySelector(minute),
           timerSeconds = document.querySelector(second);
+    
+    // while(new Date(deadline).getTime() <= new Date().getTime()) {
+    //   deadline = new Date().getTime() + 86400000;
+    // }
 
     function calcTime() {
       let currDate = new Date().getTime(),
           endTime = new Date(deadline).getTime(),
-          timeRemaining = (endTime - currDate) / 1000,
-          hours = Math.floor((timeRemaining / 60) / 60),
-          minutes = Math.floor((timeRemaining / 60) % 60), 
-          seconds = Math.floor(timeRemaining % 60);
+          timeRemaining = 24 - ((currDate / 1000 / 60 / 60) % 24 + 6),
+          // hours = Math.floor((timeRemaining / 60) / 60),
+          minutes = Math.floor((timeRemaining - Math.floor(timeRemaining)) * 60), 
+          seconds = Math.ceil(60 - (currDate / 1000 ) % 60),
+          hours = Math.floor(timeRemaining);
       
       return {seconds, minutes, hours, timeRemaining};
     }
@@ -34,12 +39,15 @@ window.addEventListener('DOMContentLoaded', () => {
         timerSeconds.textContent = '0' + timer.seconds;
       } 
       
-      if( timer.timeRemaining <= 0 ) {
-        clearInterval(intervalId);
-        timerHours.textContent = '00';
-        timerMinutes.textContent = '00';
-        timerSeconds.textContent = '00';
-      }
+      // if( timer.timeRemaining <= 0 ) {
+      //   clearInterval(intervalId);
+      //   timerHours.textContent = '00';
+      //   timerMinutes.textContent = '00';
+      //   timerSeconds.textContent = '00';
+      // }
+      
+      
+
     }
     
     let intervalId = setInterval(updateClock, 1000);
@@ -47,5 +55,5 @@ window.addEventListener('DOMContentLoaded', () => {
     intervalId;
   }
 
-timer('10 november 2019', '#timer-hours', '#timer-minutes', '#timer-seconds');
+timer('7 november 2019', '#timer-hours', '#timer-minutes', '#timer-seconds');
 });
