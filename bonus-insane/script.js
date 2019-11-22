b1a0616dbc56b5fc7ad67182f070c09b77c623fe
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-22 22:29:30 +0300

    сделал простой таск инсейн бонуса

---
+++ bonus-insane/script.js
@@ -0,0 +1,106 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use-strict';
+
+  const inputCities = document.querySelector('.input-cities'),
+      selectCities = document.getElementById('select-cities'),
+      dropdown = inputCities.querySelector('.dropdown'),
+      dropdownDefault = dropdown.querySelector('.dropdown-lists__list--default'),
+      dropdownSelect = dropdown.querySelector('.dropdown-lists__list--select'),
+      dropdownAutocomplete = dropdown.querySelector('.dropdown-lists__list--autocomplete'),
+      defaultCountryBlocks = dropdownDefault.querySelectorAll('.dropdown-lists__countryBlock'),
+      selectCountryBlocks = document.querySelectorAll('.dropdown-lists__list--select > .dropdown-lists__col > .dropdown-lists__countryBlock'),
+      autocompleteCountryBlock = dropdownAutocomplete.querySelector('.dropdown-lists__countryBlock'),
+      autocompleteNothingBlock = document.querySelector('.dropdown-lists__line--nothing');
+
+  //переключение select-блоков
+  const toggleSelectBlocks = (index) => {
+    for( let i = 0; i < selectCountryBlocks.length; i++ ) {
+      if ( index === i ) {
+        dropdownDefault.classList.remove('visible');
+        dropdownSelect.classList.add('visible');
+        selectCountryBlocks[i].classList.add('visible');
+      }
+    } 
+
+    selectCountryBlocks[index].addEventListener('click', (e) => {
+      const target = e.target.closest('.dropdown-lists__total-line');
+
+      if(target) {
+        dropdownDefault.classList.add('visible');
+        selectCountryBlocks[index].classList.remove('visible');
+        dropdownSelect.classList.remove('visible');
+      }
+    });
+  };  
+
+  //скрытие всех блоков
+  const hideBlocks = () => {
+    dropdownDefault.classList.remove('visible');
+    dropdownSelect.classList.remove('visible');
+    selectCountryBlocks.forEach(item => {
+      item.classList.remove('visible');
+    });
+    dropdownAutocomplete.classList.remove('visible');
+    autocompleteNothingBlock.classList.remove('visible');
+  };
+
+  const toggleDropdownLists = () => {
+    // открытие default блока со странами
+    inputCities.addEventListener('click', (e) => {
+      const target = e.target;
+      
+      if(target.id === 'select-cities') {
+        hideBlocks();
+        dropdownDefault.classList.add('visible');
+      } else if(target.matches('.button')) {
+        selectCities.value = '';
+        hideBlocks();
+      }  
+    });
+    
+    //открытие блока autocomplete
+    selectCities.addEventListener('input', () => {
+      if( selectCities.value !== '') {
+        let citiesArr = [...autocompleteCountryBlock.children];
+        hideBlocks();
+        dropdownAutocomplete.classList.add('visible');
+        autocompleteNothingBlock.className = 'visible'; 
+
+        citiesArr.forEach((item) => {
+            let inputVal = selectCities.value.toLowerCase(),
+            itemVal = item.children[0].textContent.toLowerCase(),
+            valLength = inputVal.length;
+            inputVal = new RegExp(`^${inputVal}`);
+            
+            
+            if( inputVal.test(itemVal.slice(0, valLength))) {
+              item.classList.add('visible');
+              autocompleteNothingBlock.className = 'hide'; 
+            } else {
+              item.classList.remove('visible');
+            }
+        });
+      } else {
+        hideBlocks();
+        dropdownDefault.classList.add('visible');
+      }
+    });
+
+    //открытие select блока с выбранной страной
+    dropdownDefault.addEventListener('click', (e) => {
+      let target = e.target.closest('.dropdown-lists__total-line');
+      
+      if(target) {
+        target = target.closest('.dropdown-lists__countryBlock');
+
+        defaultCountryBlocks.forEach((item, i) => {
+          if(target === item) {
+            toggleSelectBlocks(i);
+          }
+        });
+      } 
+    });
+  };
+
+  toggleDropdownLists();
+});
