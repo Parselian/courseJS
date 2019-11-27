4c8585353ca3fa2c053098bcb69d71e074329b84
Vyacheslav Vojdaev <parselian.study@yandex.ru>
2019-11-27 17:57:48 +0300

    test

--- script.js
+++ script.js
@@ -3,15 +3,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const cardWrap = document.querySelector('.wrap'), 
       card = document.querySelector('.card');
-
-  const addBlocks = (response) => {
-    const data = JSON.parse(response);
-
+  
+  const addBlocks = (data) => {
     for(let key in data) {
       const cloneCard = card.cloneNode('true');
-      console.log(cloneCard);
 
-      //filling card-img
+      const addInfo = (block) => {
+        switch(block.className) {
+          case 'card-img__img': 
+            // block.src = data[key].photo;
+            break;
+          case 'card-img__title':
+            block.textContent = data[key].name;
+            break;
+          case 'card-img__info':
+        }
+      }
+
+      cloneCard.childNodes.forEach((item) => {
+        if(item.className === 'card-img') {
+          for(let i = 0; i < item.children.length; i++) {
+            addInfo(item.children[i]);
+          }
+        } else if(item.className === 'card-info') {
+          for(let i = 0; i < item.children.length; i++) {
+            console.log(item.children[i].children[i]);
+            addInfo(item.children[i]);
+          }
+        }
+      });
+
+
+      // getProp(cloneCard.children);
+
+      // filling card-img
       cloneCard.children[0].children[0].src = data[key].photo;
       cloneCard.children[0].children[1].textContent = data[key].name;
       cloneCard.children[0].children[2].children[0].textContent = `${data[key].species}, ${data[key].genger}`;
@@ -26,28 +51,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
       cardWrap.appendChild(cloneCard);
     }
+    card.remove();
   };
 
   const getData = () => {
-    const request = new XMLHttpRequest();
-
-    request.open('GET', 'dbHeroes.json');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
-
-    request.addEventListener('readystatechange', () => {
-      if(request.readyState !== 4) {
-        return;
-      }
-
-      if(request.status === 200) {
-        const response = request.responseText;
-        addBlocks(response);
-      } else {
-        console.error(request.statusText);
+    return fetch('dbHeroes.json', {
+      headers: {
+        'Content-Type': 'application/json'
       }
     });
   };
 
-  getData();
+  getData()
+    .then((response) => {
+      if(response.status !== 200) {
+        throw new Error('error, network status isn`t 200');
+      }
+
+      return response.json();
+    })
+    .then((response) => {
+      addBlocks(response);
+    })
+    .catch((error) => {
+      card.remove();
+      console.error(error);
+    });
 });
